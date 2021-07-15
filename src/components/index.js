82b2b5efde5cb60163c23a7efaf1fc78f1d390e7
steffi889 <stefannyamalianov@gmail.com>
2021-07-15 20:32:06 +0700

    update homework module 2 session 2

---
+++ src/components/index.js
@@ -0,0 +1,13 @@
+function Data (props) {
+    return (
+        <div className="container">
+        <img src={props.data.album.images[1].url}/>
+           <p>Track Title: {props.data.album.name}</p>
+           <p>Artist: {props.data.album.artists[0].name}</p>
+           <p>Album: {props.data.album.album_type}</p>
+           <button onPress="">Select</button>
+        </div>
+    )
+}
+
+export default Data;
--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -1,12 +1,9 @@
 import data from "../../data/dummy";
+import Data from "../../components";
 function Album() {
     return (
-       <div class="container">
-           <img src={data.album.images[1].url}/>
-           <p>Track Title: {data.album.name}</p>
-           <p>Artist: {data.album.artists[0].name}</p>
-           <p>Album: {data.album.album_type}</p>
-           <button onPress="">Select</button>
+       <div>
+           <Data data={data}/>
        </div>
     )
 }
